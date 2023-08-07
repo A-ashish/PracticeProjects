ea5dfde9056d705bca63bdaa5a9cb41020e14055
A-ashish <A-ashish@users.noreply.github.com>
2023-08-07 20:43:01 +0530

    Delete My First core App directory

--- My First core App/My First core App/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace My_First_core_App.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
