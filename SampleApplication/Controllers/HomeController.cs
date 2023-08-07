767a8fba2ebc482ca31ff199a57f4aabfb91c538
Ashish Navghare <navghare_ashish@hotmail.com>
2023-08-07 20:04:01 +0530

    Customer CRUD

---
+++ SampleApplication/Controllers/HomeController.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SampleApplication.Models;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace SampleApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        Uri Baseaddress = new Uri("https://getinvoices.azurewebsites.net/");
+        private readonly HttpClient _Client;
+        public HomeController()
+        {
+            _Client = new HttpClient();
+            _Client.BaseAddress = Baseaddress;
+        }
+       
+        public async Task<IActionResult> Customers()
+        {
+            List<Customer> customerList = new List<Customer>();
+            _Client.DefaultRequestHeaders.Clear();
+            _Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage Res = await _Client.GetAsync("api/Customers");
+            if (Res.IsSuccessStatusCode)
+            {
+                var CustomerResponse = Res.Content.ReadAsStringAsync().Result;
+                customerList = JsonConvert.DeserializeObject<List<Customer>>(CustomerResponse);
+                return View(customerList);
+            }
+            else
+            {
+                TempData["ErrorMessage"] = Res.ReasonPhrase;
+                return View("Customers");
+            }
+        }
+        public IActionResult Create()
+        {
+            Customer customer = new Customer();
+            return View(customer);
+        }
+        [HttpPost]
+        public IActionResult Create(Customer obj)
+        {
+            Customer objCustomer = null;
+            try
+            {
+                string data = JsonConvert.SerializeObject(obj);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                _Client.DefaultRequestHeaders.Clear();
+                HttpResponseMessage Res = _Client.PostAsync("api/Customer", content).Result;
+                if (Res.IsSuccessStatusCode)
+                {
+                    var CustomerResponse = Res.Content.ReadAsStringAsync().Result;
+                    objCustomer = JsonConvert.DeserializeObject<Customer>(CustomerResponse);
+                    return View("Customers", objCustomer);
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = Res.ReasonPhrase;
+                    return View("Create");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View("Create");
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            Customer objCustomer = null;
+            _Client.DefaultRequestHeaders.Clear();
+            _Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage Res = _Client.GetAsync(_Client.BaseAddress + "api/Customer/" + id).Result;
+            if (Res.IsSuccessStatusCode)
+            {
+                var CustomerResponse = Res.Content.ReadAsStringAsync().Result;
+                objCustomer = JsonConvert.DeserializeObject<Customer>(CustomerResponse);
+            }
+            return View(objCustomer);
+        }
+        [HttpPost]
+        public IActionResult Edit(Customer obj)
+        {
+            try
+            {
+                string data = JsonConvert.SerializeObject(obj);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                _Client.DefaultRequestHeaders.Clear();
+                HttpResponseMessage Res = _Client.PutAsync(_Client.BaseAddress + "api/Customer/", content).Result;
+                if (Res.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Customers");
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = Res.ReasonPhrase;
+                    return View("Edit");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View("Edit");
+            }
+
+        }
+        
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string id)
+        {
+            Customer objCustomer = null;
+            try
+            {
+                _Client.DefaultRequestHeaders.Clear();
+                _Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage Res = await _Client.GetAsync(_Client.BaseAddress+"api/Customer/" + id);
+                if (Res.IsSuccessStatusCode)
+                {
+                    var CustomerResponse = Res.Content.ReadAsStringAsync().Result;
+                    objCustomer = JsonConvert.DeserializeObject<Customer>(CustomerResponse);
+                    return View("Delete", objCustomer);
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = Res.ReasonPhrase;
+                    return View("Delete");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View("Delete");
+            }
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int Id)
+        {
+            try
+            {
+                _Client.DefaultRequestHeaders.Clear();
+                HttpResponseMessage Res = _Client.DeleteAsync("api/Customer/" + Id).Result;
+                if (Res.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Customers");
+                }
+                else {
+                    TempData["ErrorMessage"] = Res.ReasonPhrase;
+                    return View("Delete");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View("Delete");
+            }
+        }
+
+    }
+}
---
+++ SampleApplication/Models/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleApplication.Models
+{
+    public class Customer
+    {
+        [DisplayName("CustomerId")]
+        public string Id { get; set; }
+        public string Salutation { get; set; } = "";
+        public string Initials { get; set; } = "";
+        [Required(ErrorMessage = "Please enter First Name")]
+        public string FirstName { get; set; } = "";
+        public string FirstName_Ascii { get; set; } = "";
+        [Required(ErrorMessage = "Please enter Gender")]
+        public string Gender { get; set; } = "";
+        public string Firstname_Country_Rank { get; set; } = "";
+        public string Firstname_Country_Frequency { get; set; } = "";
+        [Required(ErrorMessage = "Please enter Last Name")]
+        public string Lastname { get; set; } = "";
+        public string Lastname_ascii { get; set; } = "";
+        public string Lastname_country_rank { get; set; } = "";
+        public string Lastname_country_frequency { get; set; } = "";
+        [Required(ErrorMessage = "Please enter Email Address")]
+        [EmailAddress(ErrorMessage ="Please enter valid email address")]
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+        [Required(ErrorMessage = "Please enter Country Code")]
+        public string Country_Code { get; set; } = "";
+        public string Country_Code_Alpha { get; set; } = "";
+        public string Country_Name { get; set; } = "";
+        public string Primary_Language_Code { get; set; } = "";
+        public string Primary_Language { get; set; } = "";
+        [Required(ErrorMessage = "Please enter Balance")]
+        [DisplayName("Balance")]
+        [ConcurrencyCheck]
+        public int Balancepublic { get; set; } = 0;
+        [Required(ErrorMessage = "Phone Number")]
+        public string Phone_Number { get; set; } = "";
+        public string Currency { get; set; } = "";
+        public string timestamp { get; set; }=DateTime.Now.ToString();
+    }
+}
